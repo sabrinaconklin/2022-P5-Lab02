04a391b1e8112aa34094a33a4b08891ac3da3dbd
Sabrina <sabrina.conklin@vtsd.com>
2022-09-15 12:02:14 -0400

    80 point version complete

---
+++ Lab02.java
@@ -0,0 +1,27 @@
+// Lab02v80.java
+// The 80-point version of Lab 02 is an excercise in precisely copying a
+// provided program, followed by compiling the source code into bytecode
+// and executing the bytecode file.
+
+public class Lab02v80
+{
+    public static void main(String[] args) {
+        System.out.println("Lab02, 80 Point Version");
+        System.out.println();
+        System.out.println();
+        System.out.println(Measure" today's knowledge ");
+        System.out.println("by yesterday's confusion.");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+
+        }
+
+
+    }
+
+
+
+
+
+}
--- Lab02vst.java
+++ Lab02vst.java
@@ -1,8 +1,34 @@
-public class Lab02vst {
+// Lab02v80.java
+// The 80-point version of Lab 02 is an excercise in precisely copying a\
+// provided program, followed by compiling the source code into bytecode
+// and executing the bytecode file.
 
-    public static void main(String args[]){
-        // Example changes
+
+public class Lab02v80
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Lab 02, 80 Point Version");
+        System.out.println();
+        System.out.println();
+        System.out.print(Measure today's knowledge ");
+        System.out.println("by yesterday's confusion");
+        System.out.println();
+        System.out.print("Bewilderment + Exposure = Obvious");
+        System.out.println();
+
 
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
